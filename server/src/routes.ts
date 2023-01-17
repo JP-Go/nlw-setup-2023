8c1bb5dfaca46c26df1cbc0234917360efd52c9d
João Pedro <jpedro1580@gmail.com>
2023-01-17 20:52:41 -0300

    Reorganize routes

---
+++ server/src/routes.ts
@@ -0,0 +1,36 @@
+import { FastifyInstance } from "fastify";
+import { prisma } from "./lib/prisma";
+import { z } from "zod";
+import dayjs from "dayjs";
+
+export async function appRoutes(app: FastifyInstance) {
+  app.post("/habits", async (request) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6)),
+    });
+    const { title, weekDays } = createHabitBody.parse(request.body);
+
+    const today = dayjs().startOf("day").toDate();
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: today,
+        weekDays: {
+          create: weekDays.map((w) => {
+            return { week_day: w };
+          }),
+        },
+      },
+    });
+  });
+
+  app.get("/day", async (request) => {
+    const getDayParams = z.object({
+      date: z.coerce.date(),
+    });
+
+    const { date } = getDayParams.parse(request.query);
+  });
+}
