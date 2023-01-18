568de05a8d1da8056720fa6e85ba32dfb89a963f
João Pedro <jpedro1580@gmail.com>
2023-01-17 22:42:46 -0300

    Rename habbit to habbitDay and restyle it

---
+++ web/src/components/HabitDay.tsx
@@ -0,0 +1,9 @@
+export type HabitDayProps = {
+  completed: number;
+};
+
+export function HabitDay() {
+  return (
+    <div className="bg-zinc-900 w-10 h-10 text-white rounded-lg border-zinc-800 border-2"></div>
+  );
+}
