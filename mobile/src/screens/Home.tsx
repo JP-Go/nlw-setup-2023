faf8017ac19be5865a08cb09598cbfe7105d0d1b
João Pedro <jpedro1580@gmail.com>
2023-01-18 12:48:48 -0300

    Generate scrollable view for habitday components

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,8 +1,12 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
+import { generateDaysFromYearBeginning } from "../utils/generate-days-from-year-beginning";
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+const daysFromYearStart = generateDaysFromYearBeginning();
+const minimunSummaryDatesSize = 18 * 5;
+const ammountOfDaysToFill = minimunSummaryDatesSize - daysFromYearStart.length;
 
 export function Home() {
   return (
@@ -20,7 +24,27 @@ export function Home() {
           </Text>
         ))}
       </View>
-      <HabitDay />
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className="flex-row flex-wrap">
+          {daysFromYearStart.map((date) => (
+            <HabitDay key={date.toString()} />
+          ))}
+          {ammountOfDaysToFill > 0 &&
+            Array.from({ length: ammountOfDaysToFill }).map((_, i) => {
+              return (
+                <View
+                  key={i}
+                  className="bg-zinc-900 m-1 rounded-lg border-2 border-zinc-800 opacity-40"
+                  style={{ width: DAY_SIZE, height: DAY_SIZE }}
+                />
+              );
+            })}
+        </View>
+      </ScrollView>
     </View>
   );
 }
