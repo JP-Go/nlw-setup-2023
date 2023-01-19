abb04e71d5512575e6182945ed32aa3ccb3a242c
João Pedro <jpedro1580@gmail.com>
2023-01-19 10:42:53 -0300

    Remove header

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -8,7 +8,7 @@ import { New } from "../screens/New";
 
 export function AppRoutes() {
   return (
-    <Navigator>
+    <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="home" component={Home} />
       <Screen name="new" component={New} />
       <Screen name="habit" component={Habit} />
