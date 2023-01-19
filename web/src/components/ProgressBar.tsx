71ec527df6173619335e8da01e65e48c05fd3545
João Pedro <jpedro1580@gmail.com>
2023-01-18 22:23:16 -0300

    Create progress bar component

---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,19 @@
+type ProgressBarProps = {
+  progress: number;
+};
+
+export function ProgressBar({ progress }: ProgressBarProps) {
+  return (
+    <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+      <div
+        style={{ width: `${progress}%` }}
+        className="h-3 rounded-xl bg-violet-600 w-2/3"
+        role="progressbar"
+        arial-label="Progresso de hábitos completos nesse dia"
+        aria-valuenow={75}
+        aria-valuemin={0}
+        arial-valuemax={100}
+      />
+    </div>
+  );
+}
