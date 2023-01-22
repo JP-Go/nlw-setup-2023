e10668c0b9209de8de2b6ad2cbe4b59960c933a7
João Pedro <jpedro1580@gmail.com>
2023-01-22 14:41:12 -0300

    habilitate transitions and extract listing of habits into another component

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,37 +1,49 @@
 import * as Popover from "@radix-ui/react-popover";
-import * as CheckBox from "@radix-ui/react-checkbox";
 import { ProgressBar } from "./ProgressBar";
 import clsx from "clsx";
-import { Check } from "phosphor-react";
 import dayjs from "dayjs";
+import { HabitsList } from "./HabitsList";
+import { useState } from "react";
 
 export type HabitDayProps = {
-  completed?: number;
+  initialCompleted?: number;
   ammount?: number;
   date: Date;
 };
 
-export function HabitDay({ ammount = 0, completed = 0, date }: HabitDayProps) {
+export function HabitDay({
+  ammount = 0,
+  initialCompleted = 0,
+  date,
+}: HabitDayProps) {
+  const [completed, setCompleted] = useState(initialCompleted);
   const completedPercentage =
     ammount > 0 ? Math.round((completed / ammount) * 100) : 0;
+
+  function handleCompletedChange(completed: number) {
+    setCompleted(completed);
+  }
 
   const dayAndMonth = dayjs(date).format("DD/MM");
   const dayOfWeek = dayjs(date).format("dddd");
   return (
     <Popover.Root>
       <Popover.Trigger
-        className={clsx("w-10 h-10 text-white rounded-lg border-2", {
-          "bg-zinc-900 border-zinc-800 ": completedPercentage === 0,
-          "bg-violet-900 border-violet-700":
-            completedPercentage > 0 && completedPercentage < 20,
-          "bg-violet-800 border-violet-600":
-            completedPercentage >= 20 && completedPercentage < 40,
-          "bg-violet-700 border-violet-500":
-            completedPercentage >= 40 && completedPercentage < 60,
-          "bg-violet-600 border-violet-500":
-            completedPercentage >= 60 && completedPercentage < 80,
-          "bg-violet-500 border-violet-400": completedPercentage >= 80,
-        })}
+        className={clsx(
+          "w-10 h-10 text-white rounded-lg border-2 transition-colors focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-background",
+          {
+            "bg-zinc-900 border-zinc-800 ": completedPercentage === 0,
+            "bg-violet-900 border-violet-700":
+              completedPercentage > 0 && completedPercentage < 20,
+            "bg-violet-800 border-violet-600":
+              completedPercentage >= 20 && completedPercentage < 40,
+            "bg-violet-700 border-violet-500":
+              completedPercentage >= 40 && completedPercentage < 60,
+            "bg-violet-600 border-violet-500":
+              completedPercentage >= 60 && completedPercentage < 80,
+            "bg-violet-500 border-violet-400": completedPercentage >= 80,
+          }
+        )}
       />
       <Popover.Portal>
         <Popover.Content className="min-w-[300px] p-6 rounded-2xl flex flex-col bg-zinc-900">
@@ -40,18 +52,7 @@ export function HabitDay({ ammount = 0, completed = 0, date }: HabitDayProps) {
             {dayAndMonth}
           </span>
 
-          <div className="mt-6 flex flex-col gap-3">
-            <CheckBox.Root className="flex items-center gap-3 group">
-              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
-                <CheckBox.Indicator>
-                  <Check size={20} className="text-white" />
-                </CheckBox.Indicator>
-              </div>
-              <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
-                Beber 2L de água
-              </span>
-            </CheckBox.Root>
-          </div>
+          <HabitsList date={date} onCompletedChanged={handleCompletedChange} />
 
           <ProgressBar progress={completedPercentage} />
           <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
