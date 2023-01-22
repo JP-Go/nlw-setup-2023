9ae056ba066b71d789b9ce0a3abd69c499cb3175
João Pedro <jpedro1580@gmail.com>
2023-01-22 14:40:24 -0300

    Add transitions on progress bar

--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -7,7 +7,7 @@ export function ProgressBar({ progress }: ProgressBarProps) {
     <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
       <div
         style={{ width: `${progress}%` }}
-        className="h-3 rounded-xl bg-violet-600 w-2/3"
+        className="h-3 rounded-xl bg-violet-600 w-2/3 transition-all"
         role="progressbar"
         arial-label="Progresso de hábitos completos nesse dia"
         aria-valuenow={75}
